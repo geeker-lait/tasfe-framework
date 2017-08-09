e5c4415da9306c876690597f73e64e87d80403a1
geeker-lait <lait.zhang@gmail.com>
2017-08-09 17:16:54 +0800

    add m-s.txt

--- tasfe-crud/tasfe-crud-mysql/src/main/java/com/tasfe/framework/crud/mysql/impls/jpa/genericdao/dao/BaseDataRepository.java
+++ tasfe-crud/tasfe-crud-mysql/src/main/java/com/tasfe/framework/crud/mysql/impls/jpa/genericdao/dao/BaseDataRepository.java
@@ -45,4 +45,39 @@ public interface BaseDataRepository<T, ID extends Serializable> extends PagingAn
      */
     public Class<T> getDomainClass() ;
 
+
+
+
+    public T findOneById(String id);
+
+    public T findOneByField(String field, Object value);
+
+    public List<T> findAllByFieldEq(String field, Object value);
+
+    public List<T> findAllByFieldLike(String field, String value);
+
+    public List<T> findAllByFieldIn(String Field, Iterable<?> values);
+
+    public Integer deleteById(String id);
+
+    public Integer deleteByFieldEq(String Field, Object value);
+
+    public Integer deleteByFieldLike(String Field, String value);
+
+    public Integer deleteByFieldIn(String Field, Iterable<?> values);
+
+    public Object findOneByHql(String hql);
+
+    @SuppressWarnings("rawtypes")
+    public List findAllByHql(String hql);
+
+    public Object findOneBySql(String sql);
+
+    @SuppressWarnings("rawtypes")
+    public List findAllBySql(String sql);
+
+    public List findAllByHql(String sql, Integer max);
+
+    public List findAllByHql(String sql, Integer max, Integer startPosition);
+
 }
